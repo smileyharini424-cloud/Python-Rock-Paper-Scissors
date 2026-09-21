07ed3cb4a1c74b08d3148a923babccb043bb747c
smileyharini424-cloud <smileyharini424@gmail.com>
2026-09-20 22:48:24 -0700

    Create rock_paper_scissors.py

---
+++ rock_paper_scissors.py
@@ -0,0 +1,27 @@
+import random
+
+choices = ["rock", "paper", "scissors"]
+
+user_choice = input("Enter your choice (rock/paper/scissors): ").lower()
+
+if user_choice not in choices:
+    print("Invalid choice.")
+else:
+    computer_choice = random.choice(choices)
+
+    print("Computer's choice:", computer_choice)
+
+    if user_choice == computer_choice:
+        print("It's a tie!")
+
+    elif (
+        (user_choice == "rock" and computer_choice == "scissors")
+        or
+        (user_choice == "paper" and computer_choice == "rock")
+        or
+        (user_choice == "scissors" and computer_choice == "paper")
+    ):
+        print("You win!")
+
+    else:
+        print("Computer wins!")
